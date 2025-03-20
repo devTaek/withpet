4769f804a3a4ca11ed7831b163b8e929170036b9
d-taegyun <d.taegyun@gmail.com>
2025-03-20 14:56:00 +0900

    [ADD] 카카오맵 스크립트 로드

--- client/src/pages/Petwalk.tsx
+++ client/src/pages/Petwalk.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useEffect } from "react";
 
-const Petwalk = () => {
-  return (
-    <div className='mt-16'>
-      
-    </div>
-  )
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
 
-export default Petwalk
+const Petwalk = () => {
+  useEffect(() => {
+    let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+      level: 3, // 지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+  }, []);
+
+  return (
+    <div className="h-[calc(100vh-4rem)] mt-16 flex flex-col items-center justify-center p-10 gap-10">
+      <div className="w-full font-bold text-4xl p-2 text-center">PETSTAR</div>
+      <div
+        id="map"
+        className="w-full h-full rounded-lg border border-gray-300 shadow-md"
+      />
+    </div>
+  );
+};
+
+export default Petwalk;
